e903386f55b445c56ee22878301086d7b4e2c8bb
dmsam <dmsam@gmail.com>
2021-12-28 23:40:28 +0800

    update

--- shared/context/socket.js
+++ shared/context/socket.js
@@ -1,5 +1,5 @@
 import React from "react"
 import io from "socket.io-client";
 
-export const socket = io.connect("http://192.168.115.116:9000");
+export const socket = io.connect("http://72.167.48.154:9000");
 export const SocketContext = React.createContext();
